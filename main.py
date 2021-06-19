b9f0572057b6a57587a3b97c4cecf4f0c65b5190
ryhoh <axis38akasira@gmail.com>
2021-06-20 00:24:39 +0900

    dev

--- main.py
+++ main.py
@@ -16,19 +16,21 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 with sqlite3.connect('db/sqlite3/features.sqlite3') as conn:
     cur = conn.cursor()
     cur.execute("select code from features;")
-    available_codes: FrozenSet[str] = frozenset(i[0] for i in cur.fetchall())  # unpack tuple
+    available_utf16codes: FrozenSet[str] = frozenset(i[0] for i in cur.fetchall())  # unpack tuple
     cur.close()
 
 
 @app.get('/')
 async def root(request: Request, search_char: Optional[str] = None):
-    if search_char is None:
+    if search_char is None or search_char == "":
         return templates.TemplateResponse(
             "index.html",
             {
                 "request": request,
                 "res": None,
-                "input_char": None
+                "input_char": None,
+                "input_code": None,
+                "reccomend_end": None,
             }
         )
 
@@ -38,15 +40,20 @@ async def root(request: Request, search_char: Optional[str] = None):
             detail="Not Found"
         )
 
-    if char_to_code(search_char) not in available_codes:
+    if char_to_code(search_char, "utf-16be") not in available_utf16codes:
+        print(char_to_code(search_char, "utf-16be"))
         return templates.TemplateResponse('unavailable.html', {"request": request,})
 
+    ranking = cos_sims_ranking(search_char)
+    reccomend_end = sum(float(elm[2]) > 95 for elm in ranking[:6])  # Over 95% (6 items at most)
     return templates.TemplateResponse(
         "index.html",
         {
             "request": request,
             "res": cos_sims_ranking(search_char),
-            "input_char": search_char
+            "input_char": search_char,
+            "input_code": char_to_code(search_char, "utf-8", lower=False),
+            "reccomend_end": reccomend_end,
         }
     )
 
@@ -85,7 +92,7 @@ def select_feature_label_by_code(char_code: str) -> Tuple[np.ndarray, int]:
 
 def cos_sims_ranking(search_char: str) -> List[Tuple[str, str, str]]:
     res = []
-    char_code = char_to_code(search_char)
+    char_code = char_to_code(search_char, "utf-16be")
     char_vec: Final[np.ndarray]
     char_vec, char_label = select_feature_label_by_code(char_code)
 
@@ -98,12 +105,14 @@ def cos_sims_ranking(search_char: str) -> List[Tuple[str, str, str]]:
     return [(chr(int('0x' + code, base=16)), utf16code_to_utf8code(code), '%0.4f' % (score * 100)) for (score, code) in res[:50]]
 
 
-def char_to_code(char: str) -> str:
-    return "".join("{:02x}".format(c) for c in char.encode("utf-16be"))
+def char_to_code(char: str, enc: str, lower: bool = True) -> str:
+    if lower:
+        return "".join("{:02x}".format(c) for c in char.encode(enc))
+    return "".join("{:02X}".format(c) for c in char.encode(enc))
 
 
 def utf16code_to_utf8code(utf16code: str) -> str:
-    return "".join("{:02x}".format(c) for c in chr(int('0x' + utf16code, base=16)).encode("utf-8"))
+    return "".join("{:02X}".format(c) for c in chr(int('0x' + utf16code, base=16)).encode("utf-8"))
 
 
 if __name__ == '__main__':
